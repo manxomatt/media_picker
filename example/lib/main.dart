59dee73e1101b40607ff2f308c14be844cb9608f
skshamimul <skshamimul@gmail.com>
2018-11-15 01:22:31 +0600

    Upgraded from original media picker

---
+++ example/lib/main.dart
@@ -0,0 +1,483 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:media_picker/media_picker.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Media Picker Demo',
+      home: new MyHomePage(title: 'Media Picker Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<File> _mediaFile;
+  bool isVideo = false;
+  VideoPlayerController _controller;
+  VoidCallback listener;
+
+  void _onImageButtonPressed(ImageSource source) {
+    setState(() {
+      if (_controller != null) {
+        _controller.setVolume(0.0);
+        _controller.removeListener(listener);
+      }
+      if (isVideo) {
+        _mediaFile = MediaPicker.pickVideo(source: source).then((File _file) {
+          _controller = VideoPlayerController.file(_file)
+            ..addListener(listener)
+            ..setVolume(1.0)
+            ..initialize()
+            ..setLooping(true)
+            ..play();
+          setState(() {});
+        });
+      } else {
+        _mediaFile = MediaPicker.pickImage(source: source);
+      }
+    });
+  }
+
+  @override
+  void deactivate() {
+    if (_controller != null) {
+      _controller.setVolume(0.0);
+      _controller.removeListener(listener);
+    }
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    if (_controller != null) {
+      _controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    listener = () {
+      setState(() {});
+    };
+  }
+
+  Widget _previewVideo(VideoPlayerController controller) {
+    if (controller == null) {
+      return const Text(
+        'You have not yet picked a video',
+        textAlign: TextAlign.center,
+      );
+    } else if (controller.value.initialized) {
+      return Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: AspectRatioVideo(controller),
+      );
+    } else {
+      return const Text(
+        'Error Loading Video',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  Widget _previewImage() {
+    return FutureBuilder<File>(
+        future: _mediaFile,
+        builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.data != null) {
+            return Image.file(snapshot.data);
+          } else if (snapshot.error != null) {
+            return const Text(
+              'Error picking image.',
+              textAlign: TextAlign.center,
+            );
+          } else {
+            return const Text(
+              'You have not yet picked an image.',
+              textAlign: TextAlign.center,
+            );
+          }
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: isVideo ? _previewVideo(_controller) : _previewImage(),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () {
+              isVideo = false;
+              _onImageButtonPressed(ImageSource.gallery);
+            },
+            heroTag: 'image0',
+            tooltip: 'Pick Image from gallery',
+            child: const Icon(Icons.photo_library),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                isVideo = false;
+                _onImageButtonPressed(ImageSource.camera);
+              },
+              heroTag: 'image1',
+              tooltip: 'Take a Photo',
+              child: const Icon(Icons.camera_alt),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: FloatingActionButton(
+              backgroundColor: Colors.red,
+              onPressed: () {
+                isVideo = true;
+                _onImageButtonPressed(ImageSource.gallery);
+              },
+              heroTag: 'video0',
+              tooltip: 'Pick Video from gallery',
+              child: const Icon(Icons.video_library),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: FloatingActionButton(
+              backgroundColor: Colors.red,
+              onPressed: () {
+                isVideo = true;
+                _onImageButtonPressed(ImageSource.camera);
+              },
+              heroTag: 'video1',
+              tooltip: 'Take a Video',
+              child: const Icon(Icons.videocam),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AspectRatioVideo extends StatefulWidget {
+  final VideoPlayerController controller;
+
+  AspectRatioVideo(this.controller);
+
+  @override
+  AspectRatioVideoState createState() => new AspectRatioVideoState();
+}
+
+class AspectRatioVideoState extends State<AspectRatioVideo> {
+  VideoPlayerController get controller => widget.controller;
+  bool initialized = false;
+
+  VoidCallback listener;
+
+  @override
+  void initState() {
+    super.initState();
+    listener = () {
+      if (!mounted) {
+        return;
+      }
+      if (initialized != controller.value.initialized) {
+        initialized = controller.value.initialized;
+        setState(() {});
+      }
+    };
+    controller.addListener(listener);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (initialized) {
+      final Size size = controller.value.size;
+      return new Center(
+        child: new AspectRatio(
+          aspectRatio: size.width / size.height,
+          child: new VideoPlayPause(controller),
+        ),
+      );
+    } else {
+      return new Container();
+    }
+  }
+}
+
+class VideoPlayPause extends StatefulWidget {
+  final VideoPlayerController controller;
+
+  VideoPlayPause(this.controller);
+
+  @override
+  State createState() {
+    return new _VideoPlayPauseState();
+  }
+}
+
+class _VideoPlayPauseState extends State<VideoPlayPause> {
+  FadeAnimation imageFadeAnim =
+      new FadeAnimation(child: const Icon(Icons.play_arrow, size: 100.0));
+  VoidCallback listener;
+
+  _VideoPlayPauseState() {
+    listener = () {
+      setState(() {});
+    };
+  }
+
+  VideoPlayerController get controller => widget.controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(listener);
+    controller.setVolume(1.0);
+    controller.play();
+  }
+
+  @override
+  void deactivate() {
+    controller.setVolume(0.0);
+    controller.removeListener(listener);
+    super.deactivate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> children = <Widget>[
+      new GestureDetector(
+        child: new VideoPlayer(controller),
+        onTap: () {
+          if (!controller.value.initialized) {
+            return;
+          }
+          if (controller.value.isPlaying) {
+            imageFadeAnim =
+                new FadeAnimation(child: const Icon(Icons.pause, size: 100.0));
+            controller.pause();
+          } else {
+            imageFadeAnim = new FadeAnimation(
+                child: const Icon(Icons.play_arrow, size: 100.0));
+            controller.play();
+          }
+        },
+      ),
+      new Align(
+        alignment: Alignment.bottomCenter,
+        child: new VideoProgressIndicator(
+          controller,
+          allowScrubbing: true,
+        ),
+      ),
+      new Center(child: imageFadeAnim),
+    ];
+
+    return new Stack(
+      fit: StackFit.passthrough,
+      children: children,
+    );
+  }
+}
+
+class FadeAnimation extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+
+  FadeAnimation({this.child, this.duration: const Duration(milliseconds: 500)});
+
+  @override
+  _FadeAnimationState createState() => new _FadeAnimationState();
+}
+
+class _FadeAnimationState extends State<FadeAnimation>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        new AnimationController(duration: widget.duration, vsync: this);
+    animationController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+    animationController.forward(from: 0.0);
+  }
+
+  @override
+  void deactivate() {
+    animationController.stop();
+    super.deactivate();
+  }
+
+  @override
+  void didUpdateWidget(FadeAnimation oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.child != widget.child) {
+      animationController.forward(from: 0.0);
+    }
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return animationController.isAnimating
+        ? new Opacity(
+            opacity: 1.0 - animationController.value,
+            child: widget.child,
+          )
+        : new Container();
+  }
+}
+
+typedef Widget VideoWidgetBuilder(
+    BuildContext context, VideoPlayerController controller);
+
+abstract class PlayerLifeCycle extends StatefulWidget {
+  final VideoWidgetBuilder childBuilder;
+  final String dataSource;
+
+  PlayerLifeCycle(this.dataSource, this.childBuilder);
+}
+
+/// A widget connecting its life cycle to a [VideoPlayerController] using
+/// a data source from the network.
+class NetworkPlayerLifeCycle extends PlayerLifeCycle {
+  NetworkPlayerLifeCycle(String dataSource, VideoWidgetBuilder childBuilder)
+      : super(dataSource, childBuilder);
+
+  @override
+  _NetworkPlayerLifeCycleState createState() =>
+      new _NetworkPlayerLifeCycleState();
+}
+
+/// A widget connecting its life cycle to a [VideoPlayerController] using
+/// an asset as data source
+class AssetPlayerLifeCycle extends PlayerLifeCycle {
+  AssetPlayerLifeCycle(String dataSource, VideoWidgetBuilder childBuilder)
+      : super(dataSource, childBuilder);
+
+  @override
+  _AssetPlayerLifeCycleState createState() => new _AssetPlayerLifeCycleState();
+}
+
+abstract class _PlayerLifeCycleState extends State<PlayerLifeCycle> {
+  VideoPlayerController controller;
+
+  @override
+
+  /// Subclasses should implement [createVideoPlayerController], which is used
+  /// by this method.
+  void initState() {
+    super.initState();
+    controller = createVideoPlayerController();
+    controller.addListener(() {
+      if (controller.value.hasError) {
+        print(controller.value.errorDescription);
+      }
+    });
+    controller.initialize();
+    controller.setLooping(true);
+    controller.play();
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.childBuilder(context, controller);
+  }
+
+  VideoPlayerController createVideoPlayerController();
+}
+
+class _NetworkPlayerLifeCycleState extends _PlayerLifeCycleState {
+  @override
+  VideoPlayerController createVideoPlayerController() {
+    return new VideoPlayerController.network(widget.dataSource);
+  }
+}
+
+class _AssetPlayerLifeCycleState extends _PlayerLifeCycleState {
+  @override
+  VideoPlayerController createVideoPlayerController() {
+    return new VideoPlayerController.asset(widget.dataSource);
+  }
+}
+
+/// A filler card to show the video in a list of scrolling contents.
+Widget buildCard(String title) {
+  return new Card(
+    child: new Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new ListTile(
+          leading: const Icon(Icons.airline_seat_flat_angled),
+          title: new Text(title),
+        ),
+        new ButtonTheme.bar(
+          child: new ButtonBar(
+            children: <Widget>[
+              new FlatButton(
+                child: const Text('BUY TICKETS'),
+                onPressed: () {/* ... */},
+              ),
+              new FlatButton(
+                child: const Text('SELL TICKETS'),
+                onPressed: () {/* ... */},
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
